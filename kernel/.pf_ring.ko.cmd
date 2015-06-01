b635e8e8f5837ed71c128dcfd09e461f1d19a76d
root <root@dedi111.ek-hosting.net>
2015-06-01 16:50:37 -0500

    added updated DNA drivers

---
+++ kernel/.pf_ring.ko.cmd
@@ -0,0 +1,1 @@
+cmd_/home/bram/PF_RING/kernel/pf_ring.ko := ld -r -m elf_x86_64 -T /usr/src/linux-headers-3.16.0-4-common/scripts/module-common.lds --build-id  -o /home/bram/PF_RING/kernel/pf_ring.ko /home/bram/PF_RING/kernel/pf_ring.o /home/bram/PF_RING/kernel/pf_ring.mod.o
---
+++ kernel/.tmp_versions/pf_ring.mod
@@ -0,0 +1,2 @@
+/home/bram/PF_RING/kernel/pf_ring.ko
+/home/bram/PF_RING/kernel/pf_ring.o
